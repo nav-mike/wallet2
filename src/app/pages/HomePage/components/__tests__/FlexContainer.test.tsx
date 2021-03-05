99a0d34fe163170e414a37dec4c39b37f070d472
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2021-03-05 17:27:44 +0200

    add flex container snap

---
+++ src/app/pages/HomePage/components/__tests__/FlexContainer.test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { render } from '@testing-library/react';
+import { FlexContainer } from '../FlexContainer';
+
+describe('<FlexContainer />', () => {
+  it('should be render and match the snapshot', () => {
+    const flex = render(<FlexContainer />);
+    expect(flex.container.firstChild).toMatchSnapshot();
+  });
+});
