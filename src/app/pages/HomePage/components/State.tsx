8061f0839ee2f3be92486cc3a63137083b02e9fd
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2021-03-05 17:14:00 +0200

    add state component

--- src/app/pages/HomePage/components/State.tsx
+++ src/app/pages/HomePage/components/State.tsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { VictoryPie } from 'victory';
+import styled from 'styled-components/macro';
 
-export function State({ budget, charges }) {
+const StateDiv = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const chartStyles = {
+  labels: {
+    fontSize: 25,
+    fill: (t: any): string => {
+      const styles = { Budget: 'tomato', Charges: 'navy' };
+      return styles[t.datum.xName];
+    },
+  },
+  parent: { width: '30%' },
+};
+
+type StateProps = {
+  budget: number;
+  charges: number;
+};
+
+export function State(props: StateProps) {
+  const { budget, charges } = props;
+  const data = [
+    { x: 'Budget', y: budget },
+    { x: 'Charges', y: charges },
+  ];
   return (
-    <div>
-      <div>{budget}</div>
-      <div>{charges}</div>
-    </div>
+    <StateDiv>
+      <VictoryPie
+        data={data}
+        colorScale={['tomato', 'navy']}
+        style={chartStyles}
+      />
+    </StateDiv>
   );
 }
