d88cd8cdf9d54b17ce23e4b6854d2ef6db1da4ce
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2021-03-07 10:53:01 +0200

    fix structure

--- src/app/components/FlexContainer/__tests__/index.test.tsx
+++ src/app/components/FlexContainer/__tests__/index.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from '@testing-library/react';
-import { FlexContainer } from '../FlexContainer';
+import { FlexContainer } from '..';
 
 describe('<FlexContainer />', () => {
   it('should be render and match the snapshot', () => {
