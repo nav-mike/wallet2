d88559920eb1dc09c308d152d22f4883043f0f35
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2021-03-08 19:44:56 +0200

    add flex containers to add charge page

--- src/app/pages/AddChargePage/index.tsx
+++ src/app/pages/AddChargePage/index.tsx
@@ -3,6 +3,8 @@ import { PageWrapper } from 'app/components/PageWrapper';
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { H1 } from 'app/components/H1';
+import { FlexContainer } from 'app/components/FlexContainer';
+import { ColumnsContainer } from 'app/components/ColumnsContainer';
 
 export function AddChargePage() {
   return (
@@ -13,28 +15,32 @@ export function AddChargePage() {
       </Helmet>
       <NavBar />
       <PageWrapper>
-        <H1>Add charge</H1>
-        <form>
-          <div>
-            <label>Date of charge</label>
-            <input type="date" />
-          </div>
-          <div>
-            <label>Value of charge</label>
-            <input type="number" min="0" />
-          </div>
-          <div>
-            <label>Type of charge</label>
-            <select>
-              <option>Food</option>
-              <option>Transport</option>
-            </select>
-          </div>
-          <div>
-            <button type="button">Submit</button>
-            <a href="/">Cancel</a>
-          </div>
-        </form>
+        <FlexContainer>
+          <ColumnsContainer>
+            <H1>Add charge</H1>
+            <form>
+              <div>
+                <label>Date of charge</label>
+                <input type="date" />
+              </div>
+              <div>
+                <label>Value of charge</label>
+                <input type="number" min="0" />
+              </div>
+              <div>
+                <label>Type of charge</label>
+                <select>
+                  <option>Food</option>
+                  <option>Transport</option>
+                </select>
+              </div>
+              <div>
+                <button type="button">Submit</button>
+                <a href="/">Cancel</a>
+              </div>
+            </form>
+          </ColumnsContainer>
+        </FlexContainer>
       </PageWrapper>
     </>
   );
