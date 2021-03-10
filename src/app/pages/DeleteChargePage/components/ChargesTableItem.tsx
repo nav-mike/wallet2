964a6af4bc8d1625d71fa3656e73aeef9d462f47
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2021-03-10 17:04:09 +0200

    fix date format

--- src/app/pages/DeleteChargePage/components/ChargesTableItem.tsx
+++ src/app/pages/DeleteChargePage/components/ChargesTableItem.tsx
@@ -4,7 +4,7 @@ import { ICharge } from './ChargesTable';
 export function ChargesTableItem(props: { item: ICharge }) {
   return (
     <tr key={props.item.id}>
-      <td>{props.item.date.toString()}</td>
+      <td>{props.item.date.toLocaleDateString('en-US')}</td>
       <td>{props.item.value}</td>
       <td>{props.item.type}</td>
       <td>
