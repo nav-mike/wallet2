36560fe6cddedf436ffc4cf66b08b568f47d9d89
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2021-03-10 16:36:10 +0200

    add more html

--- src/app/pages/DeleteChargePage/index.tsx
+++ src/app/pages/DeleteChargePage/index.tsx
@@ -1,5 +1,10 @@
+import { ColumnsContainer } from 'app/components/ColumnsContainer';
+import { FlexContainer } from 'app/components/FlexContainer';
+import { NavBar } from 'app/components/NavBar';
+import { PageWrapper } from 'app/components/PageWrapper';
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
+import { H1 } from 'app/components/H1';
 
 export function DeleteChargePage() {
   return (
@@ -8,6 +13,14 @@ export function DeleteChargePage() {
         <title>Delete charge</title>
         <meta name="Delete charge page" />
       </Helmet>
+      <NavBar />
+      <PageWrapper>
+        <FlexContainer>
+          <ColumnsContainer>
+            <H1>Delete charge</H1>
+          </ColumnsContainer>
+        </FlexContainer>
+      </PageWrapper>
     </>
   );
 }
