319499d160b4dc509c9f8136a16acddaf663f73b
Mikhail Navrotskiy <m.navrotskiy@gmail.com>
2021-03-16 15:27:28 +0200

    update some snapshots

---
+++ src/app/pages/MyBudgetsPage/components/__tests__/index.test.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { createRenderer } from 'react-test-renderer/shallow';
+import { MyBudgetsPage } from '../..';
+
+const shallowRenderer = createRenderer();
+
+describe('<MyBudgetsPage />', () => {
+  it('should render and match the snapshot', () => {
+    shallowRenderer.render(<MyBudgetsPage />);
+    const renderedOutput = shallowRenderer.getRenderOutput();
+    expect(renderedOutput).toMatchSnapshot();
+  });
+});
